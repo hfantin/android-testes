b0a32a3f17b72bfb3d15efc3fc9bf7fa02676a5e
Hamilton Geraldo Fantin <hfantin@gmail.com>
2023-06-12 19:29:30 -0300

    compose parte 3 - cap 2

--- compose-parte3/app/src/main/java/com/github/hfantin/aluvery/ui/components/activities/ProdutoFormActivity.kt
+++ compose-parte3/app/src/main/java/com/github/hfantin/aluvery/ui/components/activities/ProdutoFormActivity.kt
@@ -1,15 +1,21 @@
 package com.github.hfantin.aluvery.ui.components.activities
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -20,10 +26,17 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.github.hfantin.aluvery.R
+import com.github.hfantin.aluvery.model.Product
 import com.github.hfantin.aluvery.ui.theme.AluveryTheme
+import java.lang.NumberFormatException
+import java.math.BigDecimal
 
 class ProdutoFormActivity : ComponentActivity() {
 
@@ -45,15 +58,29 @@ fun ProductFormScreen() {
     Column(
         Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .verticalScroll(rememberScrollState())
+            .padding(horizontal = 16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp),
+
     ) {
+        Spacer(modifier = Modifier)
         Text(
             text = "Criando o produto",
             Modifier.fillMaxWidth(),
             fontSize = 28.sp,
         )
         var url by remember { mutableStateOf("") }
+        if (url.isNotBlank()) {
+            AsyncImage(
+                model = url, contentDescription = null,
+                Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
+                contentScale = ContentScale.Crop,
+                placeholder = painterResource(id = R.drawable.placeholder),
+                error = painterResource(id = R.drawable.placeholder)
+            )
+        }
         TextField(
             url, onValueChange = { url = it },
             Modifier.fillMaxWidth(),
@@ -92,12 +119,21 @@ fun ProductFormScreen() {
         )
 
         Button(
-            onClick = { /*TODO*/ },
+            onClick = {
+                val convertedPrice = try {
+                    BigDecimal(price)
+                } catch (e: NumberFormatException) {
+                    BigDecimal.ZERO
+                }
+                val product = Product(name = name, price = convertedPrice, image = url, description = description)
+                Log.i("ProductFormActivity", "ProductFormScreen: $product")
+            },
             Modifier.fillMaxWidth(),
             shape = RoundedCornerShape(10)
         ) {
             Text(text = "Salvar")
         }
+        Spacer(modifier = Modifier)
     }
 }
 
