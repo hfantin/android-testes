a2d55b01c0b9660dd1fe1cced34ff4141c24371d
Hamilton Geraldo Fantin <hfantin@gmail.com>
2023-06-14 19:39:56 -0300

    compose parte 3 - cap 4

--- compose-parte3/app/src/main/java/com/github/hfantin/aluvery/ui/components/activities/MainActivity.kt
+++ compose-parte3/app/src/main/java/com/github/hfantin/aluvery/ui/components/activities/MainActivity.kt
@@ -1,30 +1,77 @@
 package com.github.hfantin.aluvery.ui.components.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import com.github.hfantin.aluvery.model.Product
-import com.github.hfantin.aluvery.sampledata.sampleProducts
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.github.hfantin.aluvery.dao.ProductDao
+import com.github.hfantin.aluvery.sampledata.sampleCandies
+import com.github.hfantin.aluvery.sampledata.sampleDrinks
 import com.github.hfantin.aluvery.sampledata.sampleSections
 import com.github.hfantin.aluvery.ui.screen.HomeScreen
 import com.github.hfantin.aluvery.ui.theme.AluveryTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val dao = ProductDao()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            App(products = sampleProducts)
+            App(onFabClick = {
+                startActivity(
+                    Intent(
+                        this,
+                        ProdutoFormActivity::class.java
+                    )
+                )
+            }) {
+                val sections = mapOf(
+                    "Todos produtos" to dao.products(),
+                    "Promoções" to sampleDrinks + sampleCandies,
+                    "Doces" to sampleCandies,
+                    "Bebidas" to sampleDrinks
+                )
+                HomeScreen(sections = sections)
+            }
         }
     }
 }
 
 @Composable
-fun App(products: List<Product>) {
+fun App(onFabClick: () -> Unit = {}, content: @Composable () -> Unit = {}) {
     AluveryTheme {
         Surface {
-            HomeScreen(sampleSections)
+            Scaffold(floatingActionButton = {
+                FloatingActionButton(onClick = onFabClick, shape = RoundedCornerShape(50.dp)) {
+                    Icon(imageVector = Icons.Default.Add, contentDescription = null)
+                }
+            }) { paddingValues ->
+                Box(modifier = Modifier.padding(paddingValues)) {
+                    content()
+                }
+            }
         }
+    }
+}
+
+@Preview
+@Composable
+fun AppPreview() {
+    App {
+        HomeScreen(sections = sampleSections)
     }
 }
